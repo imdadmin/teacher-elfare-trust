f8d037c619efa2a594dc72a2cf77ea08007d65b6
km-parvez <khaledmosharraf21@gmail.com>
2024-05-11 22:56:10 +0600

    First Commit

---
+++ src/main/java/com/khaledmosharraf/twtms/TwtmsApplication.java
@@ -0,0 +1,13 @@
+package com.khaledmosharraf.twtms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwtmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwtmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khaledmosharraf/twtms/TwtmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khaledmosharraf.twtms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwtmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
